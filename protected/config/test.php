480042a5369cb7658b182158f69374d9981678ec
Leonardo Ribeiro <leonardofribeiro@gmail.com>
2012-06-13 14:09:11 -0300

    problemas com casesensitive

--- protected/config/test.php
+++ protected/config/test.php
@@ -7,11 +7,25 @@ return CMap::mergeArray(
 			'fixture'=>array(
 				'class'=>'system.test.CDbFixtureManager',
 			),
-			/* uncomment the following to provide test database connection
 			'db'=>array(
-				'connectionString'=>'DSN for test database',
+				'connectionString' => 'mysql:host=127.0.0.1;dbname=ntiaplicacoes',
+				'emulatePrepare' => true,
+				'enableProfiling'=>true,
+				'enableParamLogging'=>true,
+				'username' => 'root',
+				'password' => 'n2t0i11',
+				'charset' => 'utf8',
 			),
-			*/
+			'log'=>array(
+			        'class'=>'CLogRouter',
+			        'routes'=>array(
+			            array(
+			                'class'=>'ext.yii-debug-toolbar.YiiDebugToolbarRoute',
+			                // Access is restricted by default to the localhost
+			                //'ipFilters'=>array('*'),
+			            ),
+			      ),
+			),
 		),
 	)
 );
