d334d105fdc3a3471f448dd1e2ec8cd4821beb0c
Leonardo Ribeiro <leonardofribeiro@gmail.com>
2012-06-22 13:48:56 -0300

    fix bug geração PDF

--- protected/controllers/RelatoriosPDFController.php
+++ protected/controllers/RelatoriosPDFController.php
@@ -318,7 +318,7 @@ class RelatoriosPDFController extends Controller
 	   "SS_RequerimentoAlunoRegistroEscolar",
 	   "SS_RequerimentoAlunoTecnico",
 	   "SS_RequerimentoAlunoGraduacao",
-	   "SS_RequerimentoAlunoEstagio"
+	   "SS_RequerimentoAlunoEstagio",
 	   "SS_RequerimentoAlunoTecnicoFG");
 	
 	   $modelReqEsp = null;
