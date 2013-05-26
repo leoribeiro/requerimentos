d1e23ad696d23ed38222db66669b2280ae486e4e
Leonardo Ribeiro <leonardofribeiro@gmail.com>
2013-05-26 14:35:42 +0200

    alteracoes segurança e layout

--- protected/views/site/_aluno.php
+++ protected/views/site/_aluno.php
@@ -23,13 +23,7 @@
 	Yii::app()->user->setFlash('info', '<div id="containerL"><div id="primaryL">'.CHtml::image($this->createUrl("images/registroescolar.png"),'').'</div><div id="contentL">'.CHtml::link($nomeR,array('Requerimentos/create',
 'form'=>'RR')).'</div></div>');
 
-	Yii::app()->user->setFlash('info', '<div id="containerL"><div id="primaryL">'.CHtml::image($this->createUrl("images/estagio.png"),'').'</div><div id="contentL">'.CHtml::link($nomeE,array('Requerimentos/create',
-		'form'=>'RE')).'</div></div>');
-?>
-
-
-
-<?php $this->widget('bootstrap.widgets.TbAlert', array(
+	$this->widget('bootstrap.widgets.TbAlert', array(
         'block'=>true, // display a larger alert block?
         'fade'=>true, // use transitions?
         'closeText'=>false, // close link text - if set to false, no close link is displayed
@@ -37,4 +31,18 @@
             'info'=>array('block'=>true, 'fade'=>true, 'closeText'=>false,'htmlOptions'=>array('style'=>'height:100px;')), // success, info, warning, error or danger
         ),
 
-)); ?>
+));
+
+	Yii::app()->user->setFlash('info', '<div id="containerL"><div id="primaryL">'.CHtml::image($this->createUrl("images/estagio.png"),'').'</div><div id="contentL">'.CHtml::link($nomeE,array('Requerimentos/create',
+		'form'=>'RE')).'</div></div>');
+
+	$this->widget('bootstrap.widgets.TbAlert', array(
+        'block'=>true, // display a larger alert block?
+        'fade'=>true, // use transitions?
+        'closeText'=>false, // close link text - if set to false, no close link is displayed
+        'alerts'=>array( // configurations per alert type
+            'info'=>array('block'=>true, 'fade'=>true, 'closeText'=>false,'htmlOptions'=>array('style'=>'height:100px;')), // success, info, warning, error or danger
+        ),
+
+	));
+?>
