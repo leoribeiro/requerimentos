5925afcc4cc50566ab2d12ad551694646edbf2ca
Leonardo Ribeiro <leonardofribeiro@gmail.com>
2013-05-26 16:59:19 +0200

    alteracoes segurança e layout

--- index-test.php
+++ index-test.php
@@ -8,10 +8,10 @@
 $yii=dirname(__FILE__).'/../../yii-1.1.13/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/test.php';
 
-require_once($yii);
-
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
 
 $projetoMarcacao = 'marcacaoprovas';
 $projetoRH = 'recursoshumanos';
