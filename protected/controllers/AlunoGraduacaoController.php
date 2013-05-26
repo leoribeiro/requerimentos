43f9022b1785ef15f85e808deeabe86fe1545ee0
Leonardo Ribeiro <leonardofribeiro@gmail.com>
2013-05-26 23:02:30 +0200

    alteracoes segurança e layout

--- protected/controllers/AlunoGraduacaoController.php
+++ protected/controllers/AlunoGraduacaoController.php
@@ -140,6 +140,8 @@ class AlunoGraduacaoController extends Controller
 					$criteria = new CDbCriteria;
 					$criteria->compare('CDAluno',$model->Aluno_CDAluno);
 					$modelAluno = Aluno::model()->find($criteria);
+					Yii::app()->user->setState('CDUsuario', $model->CDAluno);
+					Yii::app()->user->setState('novoaluno',null);
 					$this->redirect(array('//aluno/view',
 					'id'=>$modelAluno->CDAluno,'saveSuccess'=>true));
 					Yii::app()->end();
