81800959e6ef5c1b0b7cee51ace9fbee6dcbb858
Leonardo Ribeiro <leonardofribeiro@gmail.com>
2013-05-26 21:55:19 +0200

    alteracoes segurança e layout

--- protected/views/requerimentos/_formOpcoes.php
+++ protected/views/requerimentos/_formOpcoes.php
@@ -60,7 +60,19 @@ function limitText(limitField, limitCount, limitNum) {
 </div>
 <style>
 label {
-  display: inline-block;
+    display: block;
+    padding-left: 15px;
+    text-indent: -15px;
+}
+input {
+    width: 13px;
+    height: 13px;
+    padding: 0;
+    margin:0;
+    vertical-align: bottom;
+    position: relative;
+    top: -1px;
+    *overflow: hidden;
 }
 .item {white-space: nowrap;display:inline }
 .checkboxgroup{
